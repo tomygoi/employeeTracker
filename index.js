40bc95a7f752aaa03e9d30593ca41c4d87e8a1f8
Tony Trinh <tonytrinh911@gmail.com>
2023-03-27 19:11:04 -0400

    added functions for first 4 cases

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const inquirer = require("inquirer");
 const mysql = require("mysql");
+const Connection = require("mysql2/typings/mysql/lib/Connection");
 require("console.table");
 
 const db = mysql.createConnection(
@@ -11,6 +12,12 @@ const db = mysql.createConnection(
     },
     console.log('Conncted to the employeesDB database')
 );
+
+connection.connect(function(err) {
+    if (err) throw err
+    console.log("Connected as ID" + connection.threadId)
+    startPrompt();
+});
 
 function startPrompt() {
     inquirer
@@ -25,7 +32,8 @@ function startPrompt() {
             "Add a Department",
             "Add a role",
             "Add an Employee",
-            "Update an Employee Role"
+            "Update an Employee Role",
+            "End Program"
         ]
     })
     .then(function ({task}) {
@@ -51,6 +59,69 @@ function startPrompt() {
             case "Update an Employee":
                 updateEmployee();
                 break;
+            case "End Program":
+                connection.end();
+                break;
         }
     });
+}
+
+function viewDepartments() {
+    console.log("Viewing all Departments...\n");
+
+   connection.query("SELECT * FROM department", function(err, res) {
+    if (err) throw err;
+    console.table(res);
+   });
+}
+
+function viewRoles() {
+    console.log("Viewing all Roles...\n");
+    connection.query("SELECT * FROM role", function(err, res) {
+        if (err) throw err;
+        console.table(res);
+    });
+}
+
+function viewEmployees() {
+    console.log("Viewing all Employees...\n");
+    connection.query("SELECT * FROM employee", function(err, res) {
+        if (err) throw err;
+        console.table(res);
+    });
+}
+
+function addDepartment() {
+    console.log("Adding a new department...\n")
+    inquirer
+    .prompt({
+        name: "departmentName",
+        type: "input",
+        message: "What is the name of the new department?",
+    })
+    .then(function (answer) {
+        connection.query("INSERT INTO department SET ?",
+        {
+            name: answer.departmentName,
+        },
+        function (err, res) {
+            if (err) throw err;
+            console.table(res);
+            firstPrompt();
+        })
+    })
+}
+
+function addRole() {
+    console.log("Adding a new role...\n");
+    
+
+}
+
+function addEmployee() {
+
+}
+
+function updateEmployee() {
+
 }
