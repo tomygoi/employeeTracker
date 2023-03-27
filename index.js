1417bdb209ffe2692697de8b5bbeb33e41d8bfd7
Tony Trinh <tonytrinh911@gmail.com>
2023-03-26 22:07:39 -0400

    added first prompt with choices and switch cases for each choice

--- index.js
+++ index.js
@@ -11,3 +11,46 @@ const db = mysql.createConnection(
     },
     console.log('Conncted to the employeesDB database')
 );
+
+function startPrompt() {
+    inquirer
+    .prompt({
+        type: "list", 
+        name: "task", 
+        message: "What would you like to do?",
+        choices: [
+            "View All Departments",
+            "View All Roles",
+            "View all Employees",
+            "Add a Department",
+            "Add a role",
+            "Add an Employee",
+            "Update an Employee Role"
+        ]
+    })
+    .then(function ({task}) {
+        switch (task) {
+            case "View All Departments":
+                viewDepartments();
+                break;
+            case "View All Roles": 
+                viewRoles();
+                break;
+            case "View All Employees":
+                viewEmployees();
+                break;
+            case "Add a Department":
+                addDepartment();
+                break;
+            case "Add a role":
+                addRole();
+                break;
+            case "add an Employee":
+                addEmployee();
+                break;
+            case "Update an Employee":
+                updateEmployee();
+                break;
+        }
+    });
+}
